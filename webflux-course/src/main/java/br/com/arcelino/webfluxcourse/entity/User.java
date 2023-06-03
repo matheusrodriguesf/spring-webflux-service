fc99208619afd699e684d19d36659d97c40471cc
Matheus Rodrigues <matheus.arcelino@cidadania.gov.br>
2023-06-03 10:08:46 -0300

    Mapeamento da entidade && add dependencia mapstruct

---
+++ webflux-course/src/main/java/br/com/arcelino/webfluxcourse/entity/User.java
@@ -0,0 +1,25 @@
+package br.com.arcelino.webfluxcourse.entity;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+@Document
+public class User {
+
+    @Id
+    private String id;
+
+    private String name;
+
+    @Indexed(unique = true)
+    private String email;
+
+    private Integer age;
+
+}
